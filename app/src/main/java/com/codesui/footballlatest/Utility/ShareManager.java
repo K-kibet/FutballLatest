9d8252e1e42f7df9d0629de1a23e60c86d868c66
K-kibet <kkibetkkoir@gmail.com>
2025-05-15 19:13:37 +0300

    updated share-manager util

--- app/src/main/java/com/codesui/footballlatest/Utility/ShareManager.java
+++ app/src/main/java/com/codesui/footballlatest/Utility/ShareManager.java
@@ -3,6 +3,8 @@ package com.codesui.footballlatest.Utility;
 import android.content.Context;
 import android.content.Intent;
 
+import com.codesui.footballlatest.R;
+
 public class ShareManager {
     Context context;
 
@@ -13,8 +15,8 @@ public class ShareManager {
     public void shareApp() {
         Intent sendIntent = new Intent();
         sendIntent.setAction(Intent.ACTION_SEND);
-        sendIntent.putExtra(Intent.EXTRA_SUBJECT, "Powerking");
-        String shareMessage = "\nGet Today's Football Tips Here\n\n";
+        sendIntent.putExtra(Intent.EXTRA_SUBJECT, context.getString(R.string.app_name));
+        String shareMessage = "\nGet Football Livescores In Real Time\n\n";
         shareMessage = shareMessage + "https://play.google.com/store/apps/details?id=" + context.getPackageName() + "\n\n";
         sendIntent.putExtra(Intent.EXTRA_TEXT, shareMessage);
         sendIntent.setType("text/plain");
